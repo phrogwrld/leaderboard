db5309ccd91fb1b748bca781d01926926ff2dea0
Aaron Harris <aaronah02@icloud.com>
2023-03-15 16:13:55 -0400

    init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,61 @@
-<h1>You are using the o7 stack</h1>
+<script lang="ts">
+	import '../app.css';
+	const data = [
+		{
+			rank: 1,
+			lastName: 'Doe',
+			age: 25,
+		},
+		{
+			rank: 2,
+			lastName: 'Doedkhjbfgkjhdfdjkhjk',
+			age: 23,
+		},
+		{
+			rank: 3,
+			lastName: 'Smith',
+			age: 30,
+		},
+		{
+			rank: 4,
+			lastName: 'Smith',
+			age: 28,
+		},
+	];
+</script>
+
+<div
+	class="flex min-h-screen items-center justify-center bg-black font-sans text-white"
+>
+	<table class="table rounded bg-dark text-left">
+		<thead>
+			<tr>
+				<th
+					class="pl-8 pt-9 text-sm font-extralight leading-3 text-secondary subpixel-antialiased"
+					>RANKiNg</th
+				>
+				<th
+					class="pl-8 pt-9 text-sm font-extralight leading-3 text-secondary subpixel-antialiased"
+					>MEMBER</th
+				>
+				<th
+					class="pl-8 pt-9 text-sm font-extralight leading-3 text-secondary subpixel-antialiased"
+					>HOURS</th
+				>
+				<th
+					class="px-8 pt-9 text-sm font-extralight leading-3 text-secondary subpixel-antialiased"
+					>SCORE</th
+				>
+			</tr>
+		</thead>
+		<tbody>
+			{#each data as person}
+				<tr>
+					<td>{person.rank} <span> arrow</span></td>
+					<td>{person.lastName}</td>
+					<td>{person.age}</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,28 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: [
+					'Inter',
+					'system-ui',
+					'Avenir',
+					'Helvetica',
+					'Arial',
+					'sans-serif',
+				],
+				body: ['Inter', 'sans-serif'],
+				mono: ['Inter', 'sans-serif'],
+			},
+			colors: {
+				black: '#000000',
+				dark: '#181920',
+				white: '#E8E7E9',
+				secondary: '#747579',
+				green: '#91dfb8',
+				red: '#f48278',
+			},
+		},
 	},
 	plugins: [],
 };
